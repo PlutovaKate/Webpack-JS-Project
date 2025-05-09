e538a7c6c8b1978d71b4440e6d78ea3404fac573
Kateryna Plutova <plutova0207@gmail.com>
2025-05-09 22:16:00 +0300

    finish render and menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import "./styles.scss";
 
-alert("Have a nice trip");
+const menuAll = document.querySelector("#menu-all");
+const menuBurgers = document.querySelector("#menu-burgers");
+const menuChicken = document.querySelector("#menu-chicken");
+const menuPotato = document.querySelector("#menu-potato");
+const menuOrders = document.querySelector("#menu-orders");
+
+const containerBurgers = document.querySelector("#container-burgers");
+const containerChicken = document.querySelector("#container-chicken");
+const containerPotato = document.querySelector("#container-potato");
+
+const orders = [];
+
+menuAll.addEventListener("click", () => {
+  containerPotato.classList.remove("d-none");
+  containerChicken.classList.remove("d-none");
+  containerBurgers.classList.remove("d-none");
+});
+
+menuBurgers.addEventListener("click", () => {
+  containerBurgers.classList.remove("d-none");
+  containerChicken.classList.add("d-none");
+  containerPotato.classList.add("d-none");
+});
+
+menuChicken.addEventListener("click", () => {
+  containerChicken.classList.remove("d-none");
+  containerBurgers.classList.add("d-none");
+  containerPotato.classList.add("d-none");
+});
+
+menuPotato.addEventListener("click", () => {
+  containerPotato.classList.remove("d-none");
+  containerChicken.classList.add("d-none");
+  containerBurgers.classList.add("d-none");
+});
